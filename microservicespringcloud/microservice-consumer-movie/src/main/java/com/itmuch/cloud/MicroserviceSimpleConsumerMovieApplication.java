765dca51dc58e578d7b883bc0b7b347da7ab0d65
wangpenghao <wph_123456@126.com>
2018-08-27 09:18:05 +0800

    将服务注册到eureka上面

--- microservicespringcloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
+++ microservicespringcloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 public class MicroserviceSimpleConsumerMovieApplication {
 	@Bean
+	@LoadBalanced//引入Ribbon，ribbon主要就是访问eureka上面注册的服务，并且有负载均衡的功能，默认采用轮询的机制（，属于客户端的服务均衡，nginx属于服务的的负载均衡）
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- microservicespringcloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/controller/MovieController.java
+++ microservicespringcloud/microservice-consumer-movie/src/main/java/com/itmuch/cloud/controller/MovieController.java
@@ -15,9 +15,15 @@ public class MovieController {
 
   @Value("${user.userServicePath}")
   private String userServicePath;
-
+//使用原始硬编码的方式进行访问
+//  @GetMapping("/movie/{id}")
+//  public User findById(@PathVariable Long id) {
+//    return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+//  }
+  //使用ribbon进行访问，
+  //ribbon的工作原理：讲Eureka上面的注册的服务拉取一份在本地，然后根据内部封装的想应算法进行负载均衡访问
   @GetMapping("/movie/{id}")
   public User findById(@PathVariable Long id) {
-    return this.restTemplate.getForObject(this.userServicePath + id, User.class);
+    return this.restTemplate.getForObject("http://microservice-provider-user/simple/" + id, User.class);
   }
 }
